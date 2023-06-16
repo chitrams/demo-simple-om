1a56bd682b87c14c958de5af822ab04cc4e6ea8d
Aurelien Cavelan <aurelien.cavelan@unibas.ch>
2023-06-16 15:51:17 +0200

    cleanup

--- extract.R
+++ extract.R
@@ -1,16 +1,5 @@
 to_df <- function(scenarios, experiment)
 {
-    
-    #data = foreach(scenario=iter(scenarios, by='row'), .combine=rbind) %do%
-    #    {
-    #        f = paste0(experiment, "/txt/", scenario$index, ".txt")
-    #        if(file.exists(f) == FALSE)
-    #            return(NA)
-    #        d = read.csv(f, sep = "\t", header = FALSE)
-    #        d['index'] = scenario$index
-    #        d
-    #    }
-    
     n = nrow(scenarios)
     data = list()
     for (row in 1:n)
