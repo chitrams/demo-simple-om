1b5fa18cb755b8bb9faf4e1cb10edac50ed19774
chitrams <13806861+chitrams@users.noreply.github.com>
2024-09-12 16:15:33 +0800

    Revert "update paths for pawsey", updated with OM47
    This reverts commit f3a236a98af374ff8e2c06b9c1142024d91de6e2.

--- job.sh
+++ job.sh
@@ -2,23 +2,23 @@
 
 #SBATCH --job-name=OpenMalaria
 #SBATCH --time=24:00:00
-#SBATCH --account=pawsey0010
-#SBATCH --partition=work
+#SBATCH --account=penny
+#SBATCH --partition=scicore
+#SBATCH --qos=1day
 #SBATCH --output=log/%a.err #/dev/null
 #SBATCH --error=log/%a.out #/dev/null
 #SBATCH --mem=50000MB
 #SBATCH --ntasks=@NTASKS@
 #SBATCH --cpus-per-task=1
 
-##SBATCH --qos=1day
 ##SBATCH --array=@START@-@END@%1000
 
 export LMOD_DISABLE_SAME_NAME_AUTOSWAP=no
 
-ml parallel/20220522
+ml OpenMalaria/45.0-intel-compilers-2023.1.0
+ml parallel
 
-ml singularity/4.1.0-slurm
-parallel --joblog joblog.txt -j @NTASKS@ < commands.txt
+parallel --progress --joblog joblog.txt -j @NTASKS@ < commands.txt
 
 #SEEDFILE="commands.txt"
 #SEED=$(sed -n ${SLURM_ARRAY_TASK_ID}p $SEEDFILE)
