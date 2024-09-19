117a9c92725f0d9516542734267220844ff1d364
chitrams <13806861+chitrams@users.noreply.github.com>
2024-09-19 15:34:29 +0800

    Updated job script to correspond to Aurélien's

--- job.sh
+++ job.sh
@@ -2,23 +2,23 @@
 
 #SBATCH --job-name=OpenMalaria
 #SBATCH --time=24:00:00
-#SBATCH --account=penny
-#SBATCH --partition=scicore
-#SBATCH --qos=1day
+#SBATCH --account=pawsey0010
+#SBATCH --partition=work
 #SBATCH --output=log/%a.err #/dev/null
 #SBATCH --error=log/%a.out #/dev/null
-#SBATCH --mem=50000MB
+#SBATCH --mem=10G
 #SBATCH --ntasks=@NTASKS@
 #SBATCH --cpus-per-task=1
 
+##SBATCH --qos=1day
 ##SBATCH --array=@START@-@END@%1000
 
 export LMOD_DISABLE_SAME_NAME_AUTOSWAP=no
 
-ml OpenMalaria/45.0-intel-compilers-2023.1.0
-ml parallel
+ml parallel/20220522
 
-parallel --progress --joblog joblog.txt -j @NTASKS@ < commands.txt
+ml singularity/4.1.0-slurm
+parallel --joblog joblog.txt -j @NTASKS@ < commands.txt
 
 #SEEDFILE="commands.txt"
 #SEED=$(sed -n ${SLURM_ARRAY_TASK_ID}p $SEEDFILE)
